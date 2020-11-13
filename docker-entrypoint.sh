c4f965c1af6ca39d5e347c15e8738b3762f4a9e7
Ethan Nguyen <etnguyen03@hotmail.com>
2020-11-13 18:21:02 -0500

    Remove extraneous Docker PID file upon startup
    Fixes #1

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 DOCKER_HOST="/var/run/docker.sock"
+
+# Ensure that Docker's pidfile does not exist
+[ -f /var/run/docker.pid ] && rm /var/run/docker.pid
+
 containerd &> /var/log/containerd.log &
 dockerd -H unix:///var/run/docker.sock &> /var/log/dockerd.log &
 gitlab-runner run
